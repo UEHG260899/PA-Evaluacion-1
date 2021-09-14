251f92823c87b7e18e1a72f4f271028976e7f949
Uriel <urielenrique99@gmail.com>
2021-09-13 22:52:41 -0500

    agregado : registro de usuarios

--- lib/src/auth/views/registro_view.dart
+++ lib/src/auth/views/registro_view.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class RegistroView extends StatefulWidget {
   RegistroView({Key? key}) : super(key: key);
@@ -10,7 +12,6 @@ class RegistroView extends StatefulWidget {
 }
 
 class _RegistroViewState extends State<RegistroView> {
-
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   String dropDownRole = "Seleccione un rol";
 
@@ -19,6 +20,10 @@ class _RegistroViewState extends State<RegistroView> {
   TextEditingController? userController;
   TextEditingController? passController;
   TextEditingController? confPassController;
+
+  //Instancias firebase
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _dbRef = FirebaseDatabase.instance.reference();
 
   @override
   void initState() {
@@ -29,7 +34,6 @@ class _RegistroViewState extends State<RegistroView> {
     passController = new TextEditingController();
     confPassController = new TextEditingController();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -59,22 +63,31 @@ class _RegistroViewState extends State<RegistroView> {
                     child: Text(
                       'Necesitamos algunos de sus datos para continuar',
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold
-                      ),
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                   userField(),
-                  SizedBox(height: 26.0,),
+                  SizedBox(
+                    height: 26.0,
+                  ),
                   emailField(),
-                  SizedBox(height: 26.0,),
+                  SizedBox(
+                    height: 26.0,
+                  ),
                   passField(),
-                  SizedBox(height: 26.0,),
+                  SizedBox(
+                    height: 26.0,
+                  ),
                   confPassField(),
-                  SizedBox(height: 26.0,),
+                  SizedBox(
+                    height: 26.0,
+                  ),
                   roleSelect(),
-                  SizedBox(height: 26.0,),
+                  SizedBox(
+                    height: 26.0,
+                  ),
                   registroBtn(),
                 ],
               ),
@@ -85,7 +98,7 @@ class _RegistroViewState extends State<RegistroView> {
     );
   }
 
-  Widget userField(){
+  Widget userField() {
     return TextField(
       controller: userController,
       keyboardType: TextInputType.text,
@@ -109,22 +122,22 @@ class _RegistroViewState extends State<RegistroView> {
     );
   }
 
-  Widget passField(){
-  return TextField(
-    controller: passController,
-    obscureText: true,
+  Widget passField() {
+    return TextField(
+      controller: passController,
+      obscureText: true,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: 'Contraseña',
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-  );
-}
+    );
+  }
 
-  Widget confPassField(){
-  return TextField(
-    controller: confPassController,
+  Widget confPassField() {
+    return TextField(
+      controller: confPassController,
       obscureText: true,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
@@ -132,30 +145,28 @@ class _RegistroViewState extends State<RegistroView> {
           hintText: 'Confirmar contraseña',
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-  );
-}
+    );
+  }
 
-  Widget roleSelect(){
+  Widget roleSelect() {
     return DropdownButton(
       value: dropDownRole,
       underline: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(32.0)
-        ),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(32.0)),
       ),
-      onChanged: (String? nuevoRol){
+      onChanged: (String? nuevoRol) {
         setState(() {
           dropDownRole = nuevoRol!;
         });
         print(dropDownRole);
       },
-      items: <String>['Seleccione un rol','Vendedor', 'Comprador']
-        .map<DropdownMenuItem<String>>((String value){
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
+      items: <String>['Seleccione un rol', 'Vendedor', 'Comprador']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 
@@ -176,20 +187,79 @@ class _RegistroViewState extends State<RegistroView> {
     );
   }
 
-
   //Metodos aparte
 
-  registrar(){
-    //bool result = validaciones();
-    print('Hola');
+  registrar() async {
+    int result = validaciones();
+    if (result == 0) {
+      try {
+        _auth.createUserWithEmailAndPassword(
+            email: emailController!.text, password: passController!.text);
+        if (dropDownRole == 'Vendedor') {
+          _dbRef.child('vendedores').push().set({
+            'user': userController!.text,
+            'email': emailController!.text
+          }).then((value) {
+            emailController!.text = '';
+            passController!.text = '';
+            confPassController!.text = '';
+            userController!.text = '';
+            setState(() {
+              dropDownRole = 'Seleccione un rol';
+            });
+          });
+        } else {
+          _dbRef.child('compradores').push().set({
+            'user': userController!.text,
+            'email': emailController!.text
+          }).then((value) {
+            emailController!.text = '';
+            passController!.text = '';
+            confPassController!.text = '';
+            userController!.text = '';
+            setState(() {
+              dropDownRole = 'Seleccione un rol';
+            });
+          });
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak.password') {
+          customSnack('La contraseña es muy débil');
+        } else if (e.code == 'email-already-in-use') {
+          customSnack('El email ya ha sido registrado en otra cuenta');
+        }
+      }
+    } else {
+      switch (result) {
+        case 1:
+          customSnack('No puede haber campos vacios');
+          break;
+        case 2:
+          customSnack('Las contraseñas no coinciden');
+          break;
+        case 3:
+          customSnack('Es necesario elegir un rol');
+          break;
+      }
+    }
   }
 
-  bool validaciones(){
-    if(emailController!.text.isEmpty || passController!.text.isEmpty
-        || dropDownRole == 'Seleccione un rol'){
-          return false;
+  int validaciones() {
+    if (emailController!.text.isEmpty || passController!.text.isEmpty) {
+      return 1;
+    } else if (passController!.text != confPassController!.text) {
+      return 2;
+    } else if (dropDownRole == 'Seleccione un rol') {
+      return 3;
     }
 
-    return true;
+    return 0;
+  }
+
+  void customSnack(String texto) {
+    final snack = SnackBar(
+      content: Text('$texto'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snack);
   }
 }
