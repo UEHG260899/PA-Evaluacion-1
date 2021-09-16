b246fd6051f1766e5b756cb2a61a906d1aac949a
Uriel <urielenrique99@gmail.com>
2021-09-16 14:34:45 -0500

    agregado : vistas de ventas

---
+++ lib/src/vendedor/views/listar_ventas.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TabsVentas extends StatefulWidget {
+  TabsVentas({Key? key}) : super(key: key);
+
+  @override
+  _TabsVentasState createState() => _TabsVentasState();
+}
+
+class _TabsVentasState extends State<TabsVentas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
